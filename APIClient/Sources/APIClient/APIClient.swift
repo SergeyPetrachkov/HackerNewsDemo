a979117c56f5a893d306f2a65d0e182fb6ee21f4
Sergey Petrachkov <petrachkovsergey@gmail.com>
2020-10-05 11:03:44 +0300

    (QIS-9392) - [wip] combine api provider

--- APIClient/Sources/APIClient/APIClient.swift
+++ APIClient/Sources/APIClient/APIClient.swift
@@ -55,4 +55,23 @@ public enum FlatAPI {
 	return try Networking.requestData(id, functor: API.userDetails)
   }
 }
+
+import Combine
+@available(OSX 10.15, *)
+public enum CombineAPI {
+
+	static let provider = CombineRequestExecutor(session: URLSession.shared)
+	static let decoder = JSONDecoder()
+
+	public static func getJobs() -> AnyPublisher<[Int], Error> {
+		self.provider.run(.jobs(), decoder: decoder).eraseToAnyPublisher()
+	}
+
+	public static func postDetails(id: Int) -> AnyPublisher<Post, Error> {
+		self.provider.run(.post(id: id), decoder: decoder)
+	}
+
+	public static func userDetails(id: String) -> AnyPublisher<User, Error> {
+		self.provider.run(.user(id: id), decoder: decoder)
+	}
 }
---
+++ APIClient/Sources/Core/CombineRequestExecutor.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Сергей Петрачков on 05.10.2020.
+//
+
+import Foundation
+import Combine
+
+@available(OSX 10.15, *)
+public class CombineRequestExecutor {
+
+	private let urlSession: URLSession
+
+	public init(session: URLSession) {
+		self.urlSession = session
+	}
+
+	public func run<T: Decodable>(_ endpoint: APIEndpoint<T>,
+								  decoder: JSONDecoder = JSONDecoder()) -> AnyPublisher<T, Error> {
+		return self.urlSession
+			.dataTaskPublisher(for: endpoint.asURLRequest())
+			.tryMap { (data: Data, response: URLResponse) -> T in
+				let value = try decoder.decode(T.self, from: data)
+				return value
+			}
+			.receive(on: DispatchQueue.main)
+			.eraseToAnyPublisher()
+	}
+}
